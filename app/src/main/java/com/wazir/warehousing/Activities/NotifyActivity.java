e5361d4213961cf1603ba8df39ec2134904ff7a9
Akshay <waizaeer4@gmail.com>
2020-07-20 12:21:54 +0530

    14th Commit App Uses Server For its Operations

---
+++ app/src/main/java/com/wazir/warehousing/Activities/NotifyActivity.java
@@ -0,0 +1,16 @@
+package com.wazir.warehousing.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.wazir.warehousing.R;
+
+public class NotifyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notify);
+    }
+}
